caba578042a767ddb8d3d2448dae3caccaa6bfaf
javadev797982 <javadev797982@gmail.com>
2024-01-04 20:24:04 +0530

    12345:Create save Post Feature

---
+++ src/test/java/com/justtesting/justtest/JusttestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.justtesting.justtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JusttestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
